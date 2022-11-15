d70b3bb8bca5e19682d788ee26dcec044a5009af
Lee <leemyers16@gmail.com>
2022-11-14 22:52:15 -0800

    java script

--- Develop/script.js
+++ Develop/script.js
@@ -19,4 +19,44 @@
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
- 
+  $(function () {
+    // variable to show tday of week on top of page
+    let today = dayjs();
+    $('#currentDay').text(today.format('dddd, MMMM D YYYY'));
+  
+    // variable fot button
+    let saveBtn = $('.saveBtn');
+  
+    // click function on button 
+    saveBtn.on('click', function () {
+      $(this).siblings('.description').get(0).value;
+      let calenderText = $(this).siblings('.description').get(0).value;
+      localStorage.setItem($(this).parent().attr('id'), calenderText);
+    });
+  
+    // array of hours of the work day
+    let hoursOfDay = [9, 10, 11, 12, 13, 14, 15, 16];
+  
+    for (i = 0; i < hoursOfDay.length; i++) {
+      let eventStorage = localStorage.getItem('hour-' + hoursOfDay[i]);
+      let hourTextArea = $('#hour-' + hoursOfDay[i] + ' > .description');
+      hourTextArea.textContent = eventStorage;
+    }
+  
+    // variable of current hour to be able to add past present and future class
+    let currentHour = dayjs().hour();
+  
+    // for loop to add past present and future class to days of week
+    for (var i = 0; i < hoursOfDay.length; i ++) {
+      if (hoursOfDay[i] < currentHour) {
+        $('#hour-' + hoursOfDay[i]).addClass('past');
+      } else if (hoursOfDay[i] == currentHour) {
+        $('#hour-' + hoursOfDay[i]).removeClass('past');
+        $('#hour-' + hoursOfDay[i]).addClass('present');
+      } else {
+        $('#hour-' + hoursOfDay[i]).removeClass('past');
+        $('#hour-' + hoursOfDay[i]).removeClass('present');
+        $('#hour-' + hoursOfDay[i]).addClass('future');
+      }
+    }
+  });
